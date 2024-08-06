a29487a5c7f189bb9c024394b04b1a6510511491
nikezaxo <nikezaxo1@gmail.com>
2024-08-06 21:17:31 +0300

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       </head>
       <body className={inter.className}>
-        {React.cloneElement(children, { username })}
+        <div style={{ color: 'white' }}>
+          {React.cloneElement(children as React.ReactElement, { username })}
+        </div>
       </body>
     </html>
   );
